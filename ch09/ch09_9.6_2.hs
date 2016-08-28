df92ff522a0be403f751da9438cdb88e43290528
Lukas Lehner <lehner.lukas@gmail.com>
2016-08-28 17:50:32 +0300

    multipes fixes for #1

--- ch09/ch09_9.6_2.hs
+++ ch09/ch09_9.6_2.hs
@@ -1,9 +1,16 @@
-separate :: [Char] -> [[Char]]
-separate [] = []
+separate :: Char -> [Char] -> [[Char]]
+separate _ [] = []
 separate sep xs = case dropWhile (== sep) xs of
                       [] -> []
-                      az -> w : myWords rest
+                      az -> w : separate sep rest
                             where (w, rest) = break (== sep) az
+
+-- using purely takeWhile and dropWhile
+separate' :: Char -> [Char] -> [[Char]]
+separate' _ [] = []
+separate' sep xs = w : separate' sep ws
+  where w = takeWhile (/= sep) xs
+        ws = dropWhile (== sep) (drop (length w) xs)
 
 myWords :: [Char] -> [[Char]]
 myWords = separate ' '
